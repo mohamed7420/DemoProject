23d076259bec4b9b48708df6904f3bf65e2b921c
Mohamed osama <mohamed.osama9812@gmail.com>
2020-09-09 16:20:14 -0700

    Initial Commit

---
+++ DemoProject/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  DemoProject
+//
+//  Created by Mohamed on 9/9/20.
+//  Copyright © 2020 Mohamed. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
